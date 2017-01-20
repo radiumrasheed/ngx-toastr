7b71a4aa20e6960a5473b9863c6e8d7c2dd29acc
Scott Cooper <scttcper@gmail.com>
2017-01-19 20:52:44 -0800

    new gh-pages version

--- deploy/toastr.d.ts
+++
@@ -1,9 +0,0 @@
-export { Portal, PortalHost, BasePortalHost, ComponentPortal } from './portal/portal';
-export { Overlay } from './overlay/overlay';
-export { OverlayContainer } from './overlay/overlay-container';
-export { ToastContainerModule, ToastContainerDirective } from './toast-directive';
-export { OverlayRef } from './overlay/overlay-ref';
-export { Toast } from './toast-component';
-export { ToastrService, ActiveToast } from './toastr-service';
-export { ToastrConfig, ToastConfig } from './toastr-config';
-export { ToastrModule } from './toastr-module';
